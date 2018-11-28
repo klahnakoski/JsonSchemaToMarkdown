d1e5ae7ce971bd4ff03d8a8ef360f0bdea099a52
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-11-28 10:27:47 -0500

    lib updates, fixes

--- json_schema_to_markdown.py
+++ json_schema_to_markdown.py
@@ -6,46 +6,52 @@
 #
 # Author: Kyle Lahnakoski (kyle@lahnakoski.com)
 #
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
+from mo_files import File
+from mo_logs import Log, startup
 from pyLibrary import convert
-from pyLibrary.debugs import constants, startup
-from pyLibrary.debugs.logs import Log
-from pyLibrary.env.files import File
 
 
 def _convert(file_):
     if file_.is_directory():
         for f in file_.children:
             _convert(f)
-    elif file_.extension=="json":
+    elif file_.extension == "json":
         try:
             json = file_.read_json()
             if not json["$schema"]:
                 return
+            Log.note("{{json|json}}", json=json)
             md = convert.json_schema_to_markdown(json)
             file_.set_extension("md").write(md)
-        except Exception, e:
+        except Exception as e:
             Log.warning("Can not convert {{filename}}", filename=file_.name, cause=e)
 
 
 def main():
     try:
-        args = startup.argparse({
-            "name": ["--file", "--source"],
-            "help": "directory or file with *.json schema files",
-            "type": str,
-            "dest": "source",
-            "required": True
-        })
+        args = startup.argparse(
+            {
+                "name": ["--file", "--source"],
+                "help": "directory or file with *.json schema files",
+                "type": str,
+                "dest": "source",
+                "required": True,
+            }
+        )
         _convert(File(args.source))
 
-    except Exception, e:
-        Log.error("Serious problem with ActiveData service!  Shutdown completed!", cause=e)
+    except Exception as e:
+        Log.error(
+            "Serious problem with ActiveData service!  Shutdown completed!", cause=e
+        )
     finally:
         Log.stop()
+
 
 if __name__ == "__main__":
     main()
